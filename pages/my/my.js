1e3ec99440c8bca322192142aa96ef9d75f7f1ab
a0209 <870606695@qq.com>
2019-06-21 17:46:40 +0800

    订单后续处理+我的页面

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,66 +1,76 @@
-// pages/my/my.js
+import {
+  Address
+} from '../../utils/address.js';
+import {
+  Order
+} from '../order/order-model.js';
+import {
+  My
+} from '../my/my-model.js';
+
+var address = new Address();
+var order = new Order();
+var my = new My();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    pageIndex: 1
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    this._loadData();
+    this._getAddressInfo();
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 获取用户头像 昵称 订单
    */
-  onReady: function () {
+  _loadData: function() {
+    my.getUserInfo((data) => {
+      this.setData({
+        userInfo: data
+      });
+    });
 
+    this._getOrders();  // 获取历史订单
   },
 
   /**
-   * 生命周期函数--监听页面显示
+   * 获取用户地址
    */
-  onShow: function () {
-
+  _getAddressInfo: function() {
+    address.getAddress((addressInfo) => {
+      this._bindAddressInfo(addressInfo);
+    });
   },
 
   /**
-   * 生命周期函数--监听页面隐藏
+   * 绑定地址信息
    */
-  onHide: function () {
-
+  _bindAddressInfo: function(addressInfo) {
+    this.setData({
+      addressInfo: addressInfo
+    });
   },
 
   /**
-   * 生命周期函数--监听页面卸载
+   * 获取用户的所有订单
    */
-  onUnload: function () {
+  _getOrders: function() {
+    order.getOrders(this.data.pageIndex, (res) => {
+      var data = res.data;
 
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+      this.setData({
+        orderArr: data
+      });
+    });
   }
+
 })
--- pages/order/order.js
+++ pages/order/order.js
@@ -20,7 +20,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    id: -1
+    id: null
   },
 
   /**
@@ -42,6 +42,30 @@ Page({
     address.getAddress((res) => {
       this._bindAddressInfo(res);
     });
+  },
+
+  onShow: function() {
+    if (this.data.id) {
+      var that = this;
+      //下单后, 支付成功或失败后, 点左上角返回时能够更新订单状态, 所以放在onShow中
+      var id = this.data.id;
+      order.getOrderInfoById(id, (data) => {
+        that.setData({
+          orderStatus: data.status,
+          productsArr: data.snap_items,
+          account: data.total_price,
+          basicInfo: {
+            orderTime: data.create_time,
+            orderNo: data.order_no
+          },
+        });
+
+        // 快照地址
+        var addressInfo = data.snap_address;
+        addressInfo.totalDetail = address.setAddressInfo(addressInfo);
+        that._bindAddressInfo(address);
+      })
+    }
   },
 
   /**
@@ -217,52 +241,4 @@ Page({
     cart.delete(ids);
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
-  }
 })
